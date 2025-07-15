8c4ed03cf9452c43881b4c2037b7df4120887667
João Vitor Fedrizze <jh.2220@hotmail.com>
2025-07-15 00:10:56 -0300

    Inicio do projeto
    Foram inseridas alguns texto para usar de exemplos

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import "./app.scss";
+
+export default function Home() {
+  return (
+    <>
+      <header className="header">
+        <p>ICON IMG</p>
+        <p>Menu</p>
+      </header>
+      <div className="main-content">
+        <div>
+          <div style={{ color: "#1e1e1e" }}>
+            <h1 style={{ fontSize: "40px" }}>Bem vindo ao wolf lab</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              condimentum leo a massa convallis cursus.
+            </p>
+            <p>
+              Ut sit amet venenatis ante, eu tincidunt risus. Fusce purus ante,
+              rutrum eget est non, imperdiet fringilla erat. Ut ac mi et urna
+              auctor auctor.
+            </p>
+          </div>
+          <div>IMG</div>
+        </div>
+      </div>
+    </>
+  );
+}
