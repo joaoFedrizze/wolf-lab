d3e333b6e1f95168402f50f7ca2062b1bd93c995
João Vitor Fedrizze <39037326+joaoFedrizze@users.noreply.github.com>
2025-12-11 00:35:52 -0300

    Adicao da Dashboard

--- dist/splashtext/splashtext.service.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class SplashtextService {
-    private splashtexts;
-    findAll(): string[];
-}
--- src/app/services/patch.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_BASE_URL = "http://localhost:8080";
-
-export async function getPatch() {
-  const response = await axios.get(`${API_BASE_URL}/patch`);
-  return response.data;
-}
